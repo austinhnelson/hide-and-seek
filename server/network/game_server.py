1fc82e69f38684dd390be24d22fafa7092781883
Austin Nelson <nelsonhaustin@gmail.com>
2024-10-29 21:03:33 -0700

    Cooler menu, needs to be worked on as currently its just robot generated

--- server/network/game_server.py
+++ server/network/game_server.py
@@ -32,7 +32,7 @@ class GameServer:
         }
         await asyncio.to_thread(conn.send, json.dumps(welcome_message).encode())
 
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(0.05)
         await self.broadcast_player_count()
 
         try:
