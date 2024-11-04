320872ae7944f388a4e05ad6f359bd042028bdc5
Austin Nelson <nelsonhaustin@gmail.com>
2024-11-03 17:37:05 -0800

    Menu screen selection working

--- client/core/states/menu.py
+++ client/core/states/menu.py
@@ -1,4 +1,5 @@
 from .base import StateBase
+import pygame
 
 
 class MenuState(StateBase):
@@ -6,7 +7,11 @@ class MenuState(StateBase):
         self.menu = menu
 
     def handle_input(self, event):
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                self.menu.select_down()
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                self.menu.select_up()
 
     def render(self, window):
         self.menu.draw_menu(window)
--- client/ui/menu.py
+++ client/ui/menu.py
@@ -12,20 +12,39 @@ class Menu:
         self.menu_image = pygame.transform.scale(
             original_image, (DISPLAY["WIDTH"], DISPLAY["HEIGHT"]))
 
-        self.font = pygame.font.Font(None, 36)
+        self.font = pygame.font.Font(
+            "client/assets/fonts/PlayMeGames-Demo.otf", 20)
 
         self.buttons = [
-            {"text": "Join Lobby", "position": (DISPLAY["WIDTH"] // 2, 395)},
-            {"text": "How to Play", "position": (DISPLAY["WIDTH"] // 2, 475)},
-            {"text": "Exit", "position": (DISPLAY["WIDTH"] // 2, 550)}
+            {"text": "Join Lobby", "position": (
+                DISPLAY["WIDTH"] // 2, 395), "selected": True},
+            {"text": "How to Play", "position": (
+                DISPLAY["WIDTH"] // 2, 475), "selected": False},
+            {"text": "Exit", "position": (
+                DISPLAY["WIDTH"] // 2, 550), "selected": False},
         ]
+
+    def select_down(self):
+        for index, button in enumerate(self.buttons):
+            if button["selected"]:
+                button["selected"] = False
+                next_index = (index + 1) % len(self.buttons)
+                self.buttons[next_index]["selected"] = True
+                break
+
+    def select_up(self):
+        for index, button in enumerate(self.buttons):
+            if button["selected"]:
+                button["selected"] = False
+                next_index = (index - 1) % len(self.buttons)
+                self.buttons[next_index]["selected"] = True
+                break
 
     def draw_menu(self, window):
         window.blit(self.menu_image, (0, 0))
 
         for button in self.buttons:
             text_surface = self.font.render(
-                button["text"], True, (0, 0, 0))
-
+                button["text"], True, (65, 209, 84) if button["selected"] else (0, 0, 0))
             text_rect = text_surface.get_rect(center=button["position"])
             window.blit(text_surface, text_rect)
