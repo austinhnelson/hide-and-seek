8c24734a622ca1e1313ccb64598dde3909faa56d
Austin Nelson <nelsonhaustin@gmail.com>
2024-11-08 11:18:28 -0800

    Reworked game client, again no idea what I was doing

--- client/network/game_client.py
+++ client/network/game_client.py
@@ -1,67 +1,36 @@
-import asyncio
+import json
 import socket
 from dotenv import load_dotenv
 import os
-import json
+import threading
 
 load_dotenv()
 
 
 class GameClient:
     def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = socket.gethostname()
         self.port = int(os.getenv("SERVER_PORT"))
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.player_id = None
-        self.total_players = 0
-        self.player_list = []
-        self._message_queue = asyncio.Queue()
+        self.server.connect((self.host, self.port))
 
-    async def connect(self):
+        self.player_data = None
+
+        threading.Thread(target=self.__receive_data).start()
+
+    def __receive_data(self):
         try:
-            self.client_socket.connect((self.host, self.port))
-            self.client_socket.setblocking(False)
-            print("Connected to game server.")
-            asyncio.create_task(self.receive_messages())
-        except Exception as e:
-            print(f"Connection error: {e}")
-
-    async def receive_messages(self):
-        while True:
-            try:
-                data = await asyncio.to_thread(self.client_socket.recv, 1024)
-                if not data:
+            while True:
+                msg = self.server.recv(1024).decode('utf-8')
+                if msg:
+                    self.player_data = json.load(msg)
+                else:
+                    # print("Server closed connection.")
                     break
+        except Exception as ex:
+            print(f"Error receiving data: {ex}")
+        finally:
+            self.__close()
 
-                try:
-                    message = json.loads(data.decode())
-                    await self._message_queue.put(message)
-                except json.JSONDecodeError:
-                    print("Invalid JSON received")
-
-            except BlockingIOError:
-                await asyncio.sleep(0.1)
-            except Exception as e:
-                print(f"Error receiving message: {e}")
-                break
-
-    async def get_next_message(self):
-        try:
-            message = await asyncio.wait_for(self._message_queue.get(), timeout=0.1)
-            return message
-        except asyncio. TimeoutError:
-            return None
-
-    async def process_server_messages(self):
-        message = await self.get_next_message()
-        if message:
-            if message["type"] == "welcome":
-                self.player_id = message["player_id"]
-                self.total_players = message["total_players"]
-            elif message["type"] == "player_count":
-                self.total_players = message["total_players"]
-                self.player_list = message["players"]
-
-    async def disconnect(self):
-        self.client_socket.close()
-        print("Disconnected from game server.")
+    def __close(self):
+        self.server.close()
