72d3c783b6e43ebc8fb710272e1fe3de7787493b
Austin Nelson <nelsonhaustin@gmail.com>
2025-01-21 21:25:16 -0800

    draw map

--- client/ui/map.py
+++ client/ui/map.py
@@ -3,7 +3,37 @@ import pygame
 
 class Map:
     def __init__(self):
-        pass
+        self.width = 8
+        self.height = 8
+        self.map = (
+            '########'
+            '#  #   #'
+            '#   #  #'
+            '##     #'
+            '#      #'
+            '#  ##  #'
+            '#     ##'
+            '########'
+        )
+        self.tile_width = 720 // 8
+        self.tile_height = 720 // 8
+        self.border_color = (100, 100, 100)
+        self.tile_color = (118, 169, 227)
+        self.background_color = (0, 0, 0)
 
     def draw(self, window):
-        window.fill((255, 0, 0))
+        for row in range(self.height):
+            for column in range(self.width):
+                tile = row * self.width + column
+
+                # draw tiles
+                pygame.draw.rect(window,
+                                 self.border_color if self.map[tile] == '#' else self.tile_color,
+                                 (column * self.tile_width, row * self.tile_height, self.tile_width, self.tile_height))
+
+                # draw borders for tiles
+                pygame.draw.rect(window,
+                                 self.background_color,
+                                 (column * self.tile_width, row *
+                                  self.tile_height, self.tile_width, self.tile_height),
+                                 1)
